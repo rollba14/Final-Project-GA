ab9ae3b0284706b62787e0e373ce2831b00c02b6
Huan Ming Liao <hmliao14@gmail.com>
2018-08-25 20:36:25 -0700

    erased out comments

--- front-end/fp/src/app/app-routing.module.ts
+++ front-end/fp/src/app/app-routing.module.ts
@@ -23,14 +23,6 @@ const routes: Routes = [
       path: 'user',
       component: UserComponent
     },
-    // {
-    //   path: 'api/posts',
-    //   component: PostComponent
-    // },
-    // {
-    //   path: 'login/user',
-    //   component: UserComponent
-    // },
     {
       path: '**',
       component: HomeComponent
