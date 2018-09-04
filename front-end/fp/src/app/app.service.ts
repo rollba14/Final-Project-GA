efb2107e2180b3f8124b2589c257c1c683eadad8
Huan Ming Liao <hmliao14@gmail.com>
2018-09-04 01:01:14 -0700

    heroku

--- front-end/fp/src/app/app.service.ts
+++ front-end/fp/src/app/app.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppService {
-  // private baseUrl = "https://glacial-chamber-79751.herokuapp.com";
-  private baseUrl = "http://localhost:3000";
+  private baseUrl = "https://glacial-chamber-79751.herokuapp.com";
+  // private baseUrl = "http://localhost:3000";
 
   // This is a basic service for configuring all
   // global constant that change baseUrl depending on
